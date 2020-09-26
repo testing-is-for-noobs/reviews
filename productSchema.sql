14322362010f7dc5bc83590714be3b213f7c8f28
songkenny <songkenny95@gmail.com>
2020-09-26 12:24:30 -0700

    PULL REQUEST2: update products table to include percentages for ratings and recommendation

--- productSchema.sql
+++ productSchema.sql
@@ -13,10 +13,17 @@
 -- );
 USE reviews;
 
-CREATE TABLE products AS (SELECT pid, ROUND(avg(rating),2) AS avg_score, count(id) AS total_reviews,
+CREATE TABLE products AS (
+SELECT pid, ROUND(avg(rating),2) AS avg_score, count(id) AS total_reviews,
+ROUND(count(if (recommendationYN = 'Y', 1, NULL))/count(id) * 100, 0) AS recommendation_percentage,
 count(if (rating = 5, 1, NULL)) AS five_star, count(if (rating = 4, 1, NULL)) AS four_star,
 count(if (rating = 3, 1, NULL)) AS three_star, count(if (rating = 2, 1, NULL)) AS two_star,
 count(if (rating = 1, 1, NULL)) AS one_star, ROUND(avg(play_experience),2) AS play_experience_avg,
-ROUND(avg(difficulty_level),2) AS difficulty_level_avg, ROUND(avg(money_value),2) AS money_value_avg
+ROUND(avg(difficulty_level),2) AS difficulty_level_avg, ROUND(avg(money_value),2) AS money_value_avg,
+ROUND(count(if (rating = 5, 1, NULL))/count(id), 2) AS five_star_percentage,
+ROUND(count(if (rating = 4, 1, NULL))/count(id), 2) AS four_star_percentage,
+ROUND(count(if (rating = 3, 1, NULL))/count(id), 2) AS three_star_percentage,
+ROUND(count(if (rating = 2, 1, NULL))/count(id), 2) AS two_star_percentage,
+ROUND(count(if (rating = 1, 1, NULL))/count(id), 2) AS one_star_percentage
 FROM reviews
 GROUP BY pid);
