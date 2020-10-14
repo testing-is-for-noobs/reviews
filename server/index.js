9f38877b3ec17f3f9a2f1416da0e829c808f0f0a
Aubrey Ling <aubreylting@gmail.com>
2020-10-14 10:54:53 -0700

    reademe.md

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const port = 3000;
 
 app.use(express.static(`${__dirname}/../client/dist`));
 
-app.get('/:pid/reviews', (req, res) => {
+app.get('/reviews/:pid', (req, res) => {
   db.getReviews(req.params.pid).then(([reviews, products]) => {
     const data = { reviews, products };
     res.status(202).send(data);
@@ -15,13 +15,25 @@ app.get('/:pid/reviews', (req, res) => {
   });
 });
 
-app.put('/:pid/reviews/vote/:voteType/:id/:toggle', (req, res) => {
-  db.vote(req.params.voteType, req.params.id, req.params.toggle).then((data) => {
-    res.status(202).send(data);
-  }).catch((err) => {
-    res.status(404).send(err);
-  });
+app.post('/reviews/:pid', (req, res) => {
+  res.status(201).send('Post request');
 });
+
+app.put('/reviews/helpful/:pid', (req, res) => {
+  res.status(202).send('Put request');
+});
+
+app.delete('/reviews/:pid/:uid', (req, res) => {
+  res.status(202).send('Delete request');
+});
+
+// app.put('/:pid/reviews/vote/:voteType/:id/:toggle', (req, res) => {
+//   db.vote(req.params.voteType, req.params.id, req.params.toggle).then((data) => {
+//     res.status(202).send(data);
+//   }).catch((err) => {
+//     res.status(404).send(err);
+//   });
+// });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
